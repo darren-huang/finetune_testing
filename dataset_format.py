ec7034c02cf0f290ffd5fa045a23b603cab2df00
Darren Huang <darren.y.huang@berkeley.edu>
2019-11-19 22:58:59 -0800

    update dataset formatter

--- dataset_format.py
+++ dataset_format.py
@@ -20,28 +20,29 @@ name_set = "caltech101"
 root_data_loc = "data/" + name_set
 data_dir = "data/" + name_set + "_all"
 train = "data/" + name_set + "/train"
-test = "data/" + name_set + "/test"
+val = "data/" + name_set + "/val"
 train_frac = 0.8
 
 if __name__ == '__main__':
 
     # delete / create folders
     print("deleting and then creating folders")
-    if not os.path.exists(root_data_loc): # root must exist
-        os.mkdir(root_data_loc)
+    if os.path.exists(root_data_loc): # reset root
+        shutil.rmtree(root_data_loc)
     if os.path.exists(train): # rmv old
         shutil.rmtree(train)
-    if os.path.exists(test):
-        shutil.rmtree(test)
+    if os.path.exists(val):
+        shutil.rmtree(val)
+    os.mkdir(root_data_loc) # make everything again
     os.mkdir(train)
-    os.mkdir(test)
+    os.mkdir(val)
 
     # iter over categories/labels
     for label in os.listdir(data_dir):
         print(f"processing label: {label}")
         label_path = os.path.join(data_dir, label)
         os.mkdir(path.join(train,label))
-        os.mkdir(path.join(test, label))
+        os.mkdir(path.join(val, label))
         if os.path.isdir(label_path): # check is directory
             files = [file for file in os.listdir(label_path)]
             random.shuffle(files)
@@ -50,5 +51,5 @@ if __name__ == '__main__':
                 dst =  path.join(path.join(train, label), f)
                 shutil.copyfile(path.join(label_path, f), dst)
             for f in files[numTrain:]:
-                dst = path.join(path.join(test, label), f)
+                dst = path.join(path.join(val, label), f)
                 shutil.copyfile(path.join(label_path, f), dst)
